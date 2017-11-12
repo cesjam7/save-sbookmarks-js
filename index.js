56b3a4dfa3953360e2902b809c0047905e983043
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2017-11-12 01:52:27 -0500

    instalando mongoose y conectandome a mongo

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 'use strict'
 
+var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3678;
 
-app.listen(port, () => {
-    console.log(`API REST FAVORITOS funcionando en http://localhost:${port}`);
-})
+// puerto por default 27017
+// tengo una base de datos ya creada llamada 'cursofavoritos'
+mongoose.connect('mongodb://localhost:27017.cursofavoritos', (error, response) => {
+
+    if (error) {
+        throw error;
+    } else {
+        console.log('Conexion a MongoDB correcta');
+        app.listen(port, () => {
+            console.log(`API REST FAVORITOS funcionando en http://localhost:${port}`);
+        })
+    }
+
+});
