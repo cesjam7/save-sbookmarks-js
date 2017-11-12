7e0835d461827c337e8c529865da742ade984d73
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2017-11-12 01:56:11 -0500

    creando Schema

---
+++ models/favorito.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var mongoose = require('mongoose');
+// esto permite definir schemas u objetos de tipo schema para trabajar con un tipo de dato concreto
+var Schema = mongoose.Schema;
+
+var FavoritosSchema = Schema({
+    title : String,
+    description : String,
+    url : String
+});
+
+module.exports = mongoose.model('Favorito', FavoritoSchema);
