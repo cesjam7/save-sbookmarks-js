b6a720ca254a0cf3bb9e7b8efaddb91010b737f0
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2017-11-12 00:51:05 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var express = require('express');
+
+const app = express();
+
+app.listen(3678, function() {
+    console.log('API REST FAVORITOS funcionando en http://localhost:3768');
+})
