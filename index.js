7c020af3e84b3ef832932888eae1dac775333774
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2017-11-12 01:07:27 -0500

    rutas y parametros

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ var port = process.env.PORT || 3678;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// Si ponemos un ? al final, hacemos que ese parametro personalizado sea opcional
+app.get('/prueba/:nombre?', function(request, response) {
+    if (request.params.nombre) {
+        var nombre = request.params.nombre;
+    } else {
+        var nombre = 'sin nombre';
+    }
+    response.status(200).send({
+        text: 'Hola '+nombre
+    });
+})
+
 app.listen(port, function() {
     console.log(`API REST FAVORITOS funcionando en http://localhost:${port}`);
 })
